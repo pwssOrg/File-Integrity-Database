3216b77eaf85a811ac2fc12f0a16ffcd9cf01f2f
pwgit-create <snow_900@outlook.com>
2025-08-15 00:53:46 +0200

    BigInt (Long) -> Integer (User_Logintables

--- database/tables/user_login/user.sql
+++ database/tables/user_login/user.sql
@@ -1,7 +1,6 @@
 CREATE TABLE IF NOT EXISTS "user_"(
 id SERIAL PRIMARY KEY,
 username TEXT UNIQUE NOT NULL,
-auth_id bigint REFERENCES auth(id) NOT NULL,
-user_session_id bigint REFERENCES user_session(id),
-user_time bigint REFERENCES "time"(id) NOT NULL
+auth_id int REFERENCES auth(id) NOT NULL,
+user_time int REFERENCES "time"(id) NOT NULL
 );
