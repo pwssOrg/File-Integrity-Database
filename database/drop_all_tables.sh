b1ee7eaf947081349590fcb4138b1d457e43d386
pwgit-create <snow_900@outlook.com>
2025-08-15 04:33:04 +0200

    Update Readme and addition to sh Drop Script

--- database/drop_all_tables.sh
+++ database/drop_all_tables.sh
@@ -5,3 +5,7 @@ psql -d integrity_hash -c "DROP TABLE scan CASCADE;"
 psql -d integrity_hash -c "DROP TABLE monitored_directory CASCADE;"
 psql -d integrity_hash -c "DROP TABLE checksum CASCADE;"
 psql -d integrity_hash -c "DROP TABLE file CASCADE;"
+
+psql -d integrity_hash -c "DROP TABLE user_ CASCADE;"
+psql -d integrity_hash -c "DROP TABLE auth CASCADE;"
+psql -d integrity_hash -c "DROP TABLE time CASCADE;"
