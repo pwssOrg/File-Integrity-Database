6542dbbf203ccba77e6dcbe30206de52f4a82fa6
Peter Westin <83552499+pwgit-create@users.noreply.github.com>
2025-08-17 22:19:27 +0200

    Update scan_summary.sql
    Change datatype from Integer -> BigInt

--- database/tables/scan_summary.sql
+++ database/tables/scan_summary.sql
@@ -1,5 +1,5 @@
 CREATE TABLE scan_summary (
-    id SERIAL PRIMARY KEY,
+    id BIGSERIAL PRIMARY KEY,
     scan_id INTEGER NOT NULL REFERENCES scan(id),
     file_id bigint NOT NULL REFERENCES file(id),
     checksum_id bigint NOT NULL REFERENCES checksum(id)
