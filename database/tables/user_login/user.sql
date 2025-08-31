c354e1d38c19ffddf6487fe7534e05b7b8fcc66d
pwgit-create <snow_900@outlook.com>
2025-09-01 01:19:05 +0200

    Datatype change for foreign key time_id

--- database/tables/user_login/user.sql
+++ database/tables/user_login/user.sql
@@ -2,5 +2,5 @@ CREATE TABLE IF NOT EXISTS "user_"(
 id SERIAL PRIMARY KEY,
 username TEXT UNIQUE NOT NULL,
 auth_id int REFERENCES auth(id) NOT NULL,
-user_time int REFERENCES "time"(id) NOT NULL
+user_time bigint REFERENCES "time"(id) NOT NULL
 );
